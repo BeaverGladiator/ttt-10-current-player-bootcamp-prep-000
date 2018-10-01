4f6aee138c20d972c6b120e43ab575dd09de532a
Matthew Migliore <mmigliore311@gmail.com>
2018-10-01 11:50:34 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,6 +2,10 @@ turns_played = 0
 
 def turn_count(board)
   board.each do |position|
-    if 
+    if position != " "
+      turns_played +=
+    end
   end
+  
+  puts "#{turns_played} turns have been played"
 end
